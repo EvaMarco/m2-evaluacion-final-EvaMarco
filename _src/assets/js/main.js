6695d43fdb1b6076c86b7c42774c77df4ee75681
Migara <evamarcod@gmail.com>
2019-08-08 09:56:24 +0200

    Add simple favorite functionality

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -8,6 +8,46 @@ const btn = document.querySelector('.js__searchBtn');
 const favContainer = document.querySelector('.js__fav');
 const resultsContainer = document.querySelector('.js__results');
 const resultList = [];
+const favs = [];
+
+function createNewElement(tag, myClass, id, name){
+  const newShow = document.createElement(tag);
+  newShow.classList.add(myClass);
+  newShow.setAttribute('data-id', id);
+  const showTitle = document.createElement('h3');
+  const titleText = document.createTextNode(name);
+  showTitle.appendChild(titleText);
+  newShow.appendChild(showTitle);
+  return newShow;
+}
+
+function createNewImage(tag, myClass, src, alt){
+  const showImage = document.createElement(tag);
+  showImage.src = src;
+  showImage.alt = alt;
+  showImage.classList.add(myClass);
+  return showImage;
+}
+
+function addFavs(event){
+  const item = event.currentTarget;
+  const name = item.getAttribute('data-id');
+  item.classList.toggle('show__fav');
+  if(item.classList.contains('show__fav')){
+    if(favs.includes(name) === false){
+      // Añadirlo a la lista
+      favs.push(name);
+    }
+  }
+  else{
+    // Eliminarlo de la lista
+    const index = favs.indexOf(name);
+    if(index > -1){
+      favs.splice(index, 1);
+    }
+  }
+  localStorage.setItem('arrayFav', JSON.stringify(favs));
+}
 
 function search(){
   const inputShow = inputText.value;
@@ -16,31 +56,25 @@ function search(){
     .then(response => response.json())
     .then(data => {
       for(const item of data){
-        const newShow = document.createElement('div');
-        newShow.classList.add(`show`);
-        newShow.setAttribute('data-id', `${item.show.id}`);
-        const showTitle = document.createElement('h3');
-        const titleText = document.createTextNode(item.show.name);
-        showTitle.appendChild(titleText);
-        newShow.appendChild(showTitle);
+        const newShow = createNewElement('div', 'show', `${item.show.id}`, item.show.name);
         if(item.show.image !== null){
-          const showImage = document.createElement('img');
-          showImage.src = item.show.image.medium;
-          showImage.alt = item.show.name;
-          showImage.classList.add('img');
+          const showImage = createNewImage('img', 'img', item.show.image.medium, item.show.title);
           newShow.appendChild(showImage);
         }
         else{
-          const showImage = document.createElement('img');
-          showImage.src = 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
-          showImage.alt = item.show.name;
-          showImage.classList.add('img');
+          const imgDefualt = 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
+          const showImage =createNewImage('img', 'img', imgDefualt, item.show.title);
           newShow.appendChild(showImage);
         }
         resultList.push(newShow);
       }
       for(const item of resultList){
         resultsContainer.appendChild(item);
+      }
+      console.log(resultList);
+      const shows = document.querySelectorAll('.show');
+      for(const item of shows){
+        item.addEventListener('click', addFavs);
       }
     });
 }
