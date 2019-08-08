c3df1b6ab634a6231198844ed4d2c85401beeaf8
Migara <evamarcod@gmail.com>
2019-08-08 16:59:47 +0200

    Add close button to favs

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -7,6 +7,7 @@ const inputText = document.querySelector('.js__inputText');
 const btn = document.querySelector('.js__searchBtn');
 const favContainer = document.querySelector('.js__fav');
 const resultsContainer = document.querySelector('.js__results');
+const resetBtn = document.querySelector('.reset__btn');
 let resultList = [];
 let favs = [];
 let favsarray = JSON.parse(localStorage.getItem('favoriteShowsArray'));
@@ -24,6 +25,19 @@ function createNewElement(tag, myClass, id, name) {
   showTitle.appendChild(titleText);
   newShow.appendChild(showTitle);
   return newShow;
+}
+function createNewDiv(tag, myClass, id) {
+  const newDiv = document.createElement(tag);
+  newDiv.classList.add(myClass);
+  newDiv.setAttribute('data-id', id);
+  return newDiv;
+}
+function createNewTitle(tag, myClass, name) {
+  const newTitle = document.createElement(tag);
+  newTitle.classList.add(myClass);
+  const titleText = document.createTextNode(name);
+  newTitle.appendChild(titleText);
+  return newTitle;
 }
 
 function createNewImage(tag, myClass, src, alt) {
@@ -70,13 +84,34 @@ function addFavs(event) {
 function saveToLocalStorage() {
   localStorage.setItem('favoriteShowsArray', JSON.stringify(favs));
 }
+function remove(event){
+  const guilty = event.currentTarget;
+  const parentG = guilty.parentElement;
+  const granPG = parentG.parentElement;
+  const granPgId =granPG.getAttribute('data-id');
+  let favorite = favs.find(favItem => favItem['id'] === granPgId);
+  const index = favs.indexOf(favorite);
+  if (index > -1) {
+    favs.splice(index, 1);
+  }
+  saveToLocalStorage();
+  writeFav();
+}
 function writeFav() {
   favContainer.innerHTML = '';
   let favsarray = JSON.parse(localStorage.getItem('favoriteShowsArray'));
   if (favsarray !== null) {
     for (const item of favsarray) {
-      const favShow = createNewElement('div', 'favShow', item.id, item.name);
+      const favTitleDiv = createNewDiv('div', 'fav_TitleWrapper', item.id);
+      const favTitle = createNewTitle('h3', 'fav__ShowTitle', item.name);
+      const closeBtn = document.createElement('button');
+      closeBtn.classList.add('closeBtn');
+      closeBtn.addEventListener('click', remove);
+      favTitleDiv.appendChild(favTitle);
+      favTitleDiv.appendChild(closeBtn);
       const favImage = createNewImage('img', 'favImg', item.img, item.name);
+      const favShow = createNewDiv('div', 'favShow', item.id);
+      favShow.appendChild(favTitleDiv);
       favShow.appendChild(favImage);
       favContainer.appendChild(favShow);
     }
@@ -97,6 +132,7 @@ function search() {
       resultList = [];
       for (const item of data) {
         const newShow = createNewElement('div', 'show', `${item.show.id}`, item.show.name);
+
         if (item.show.image !== null) {
           const showImage = createNewImage('img', 'img', item.show.image.medium, item.show.title);
           newShow.appendChild(showImage);
@@ -113,7 +149,7 @@ function search() {
         if (favs !== null && favs.length !== 0) {
           let favorite = favs.find(favItem => favItem['id'] === newid);
           if (favorite !== undefined) {
-            item.classList.add('show__fav');
+            item.classList.toggle('show__fav');
           }
         }
         resultsContainer.appendChild(item);
@@ -129,9 +165,18 @@ function fakeClick(event) {
     search();
   }
 }
+function cleanAll(){
+  favs=[];
+  localStorage.clear();
+  writeFav();
+  const cleanArray = document.querySelectorAll('.show__fav');
+  for(const i of cleanArray){
+    i.classList.toggle('show__fav');
+  }
+}
 
 inputText.addEventListener('keyup', fakeClick);
-
+resetBtn.addEventListener('click', cleanAll);
 btn.addEventListener('click', search);
 
 init();
