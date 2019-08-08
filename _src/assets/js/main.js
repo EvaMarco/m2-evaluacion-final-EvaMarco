298b2a01441e308e574db51bd9b3016f875e3859
Migara <evamarcod@gmail.com>
2019-08-08 08:58:58 +0200

    Add Constants and listeners in JS

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
+// eslint-disable-next-line no-console
 console.log('>> Ready :)');
+
+const inputText = document.querySelector('.js__inputText');
+const btn = document.querySelector('.js__searchBtn');
+const favContainer = document.querySelector('.js__fav');
+const resultsContainer = document.querySelector('.js__results');
+const inputShow = inputText.nodeValue;
+
+const ENDPOINT = `http://api.tvmaze.com/search/shows?q=${inputShow}`;
+
+function search(){
+  console.log('busco');
+}
+
+btn.addEventListener('click', search);
